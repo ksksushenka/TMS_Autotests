35687f9ed101df52bb3d134684d82ca27a14089f
Kseniya Grebenyuk <kgrebenyuk@caeonline.com>
2023-04-18 05:49:28 -0500

    Added tasks for lesson2

---
+++ TMS_Autotest2/TMS_Autotest2/Lesson2/DropdownTest.cs
@@ -0,0 +1,57 @@
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Support.UI;
+
+namespace TMS_Autotest2.Lesson2
+{
+    internal class DropdownTest
+    {
+        WebDriver Chrome { get; set; }
+
+        [SetUp]
+        public void Setup()
+        {
+            Chrome = new ChromeDriver();
+            Chrome.Manage().Window.Maximize();
+            Chrome.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(15);
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Chrome.Navigate().GoToUrl("http://the-internet.herokuapp.com/dropdown");
+
+            IWebElement dropdown = Chrome.FindElement(By.Id("dropdown"));
+            dropdown.Click();
+            List<IWebElement> elements = Chrome.FindElements(By.TagName("option")).ToList();
+
+            foreach(IWebElement element in elements)
+            {
+                var displayed = element.Displayed;
+                Assert.IsTrue(displayed);
+            }
+
+            SelectElement selected = new SelectElement(Chrome.FindElement(By.Id("dropdown")));
+            selected.SelectByText("Option 1");
+            IWebElement element1 = Chrome.FindElement(By.XPath("//option[@value='1']"));
+            var checkElement1 = element1.GetAttribute("selected");
+            Assert.IsNotNull(checkElement1);
+
+            selected.SelectByText("Option 2");
+            IWebElement element2 = Chrome.FindElement(By.XPath("//option[@value='2']"));
+            var checkElement2 = element2.GetAttribute("selected");
+            Assert.IsNotNull(checkElement2);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Chrome.Quit();
+        }
+    }
+}
---
+++ TMS_Autotest2/TMS_Autotest2/Lesson2/InputsTest.cs
@@ -0,0 +1,59 @@
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TMS_Autotest2.Lesson2
+{
+    internal class InputsTest
+    {
+        WebDriver Chrome { get; set; }
+
+        [SetUp]
+        public void Setup()
+        {
+            Chrome = new ChromeDriver();
+            Chrome.Manage().Window.Maximize();
+            Chrome.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(15);
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Chrome.Navigate().GoToUrl("http://the-internet.herokuapp.com/inputs");
+
+            IWebElement input = Chrome.FindElement(By.TagName("input"));
+
+            input.SendKeys("123456789");
+            var text1 = input.Text;
+            Assert.IsNotNull(text1);
+
+            input.Clear();
+            input.Click();
+            input.SendKeys("asdsad");
+            var text2 = input.Text;
+            Assert.IsEmpty(text2);
+
+            input.Clear();
+            input.SendKeys(Keys.ArrowUp);
+            var text3 = input.Text;
+            Assert.IsNotNull(text3);
+
+            input.Clear();
+            input.SendKeys(Keys.ArrowDown);
+            var text4 = input.Text;
+            Assert.IsNotNull(text4);
+
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Chrome.Quit();
+        }
+    }
+}
